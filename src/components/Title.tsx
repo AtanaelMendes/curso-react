9b2bc037d4341e524f8a80070ee93c65f0f3a87f
atanaelmendes <programadormendes@gmail.com>
2022-10-27 10:10:51 -0300

    cliente

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,10 +2,10 @@
 const Title = (props) => {
     return (
         <div className={`flex flex-col justify-center`}>
-            <h1 className="px-5 py-2">
+            <h1 className="px-5 py-2 text-2xl">
                 {props.children}
             </h1>
-            <hr />
+            <hr className="border-2 border-purple-500"/>
         </div>
     );
 }
